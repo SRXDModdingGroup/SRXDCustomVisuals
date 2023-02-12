1662f631505dcc56741eb9a62172ae0884f153ad
Dominic Aglialoro <domiaglialoro@gmail.com>
2023-02-11 21:34:40 -0800

    Version

--- SRXDCustomVisuals.Plugin/PluginInfo.cs
+++ SRXDCustomVisuals.Plugin/PluginInfo.cs
@@ -3,5 +3,5 @@
 public static class PluginInfo {
     public const string Guid = "SRXD.CustomVisuals";
     public const string Name = "CustomVisuals";
-    public const string Version = "1.1.0";
+    public const string Version = "1.2.0";
 }
